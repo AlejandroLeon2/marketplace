0df0bb568b3958f2ae2ca9e8c1fcc61f919222e7
antony.sl <antony.g.s.lopez@gmail.com>
2025-08-26 22:04:37 -0500

    ruta de public/MasVendidos corregido  a src//src/components/MasVendidos/indexMV

---
+++ frontend/src/components/MasVendidos/indexMV.ts
@@ -0,0 +1,10 @@
+
+import { showMasVendidos } from "./Script/core";
+import { eventoMasVendido } from "./Script/listener";
+
+
+export function indexMasVendidos():void{
+
+    showMasVendidos();
+    eventoMasVendido();
+}
