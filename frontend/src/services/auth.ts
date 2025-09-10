c7d942954ce8b6fb87186567e998cca8cb75a1ad
Pedro Alejandro <alejandroleonpedro7@gmail.com>
2025-09-10 01:52:30 -0500

    arreglando inicio de seccion

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -12,38 +12,54 @@ import { auth, db } from "./firebase";
 
 const googleProvider = new GoogleAuthProvider();
 
+
 export const signInWithGoogle = async () => {
-    try {
-        await signInWithPopup(auth, googleProvider);
-    } catch (err) {
-        console.error("Error signing in with Google", err);
-    }
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    const user = result.user;
+
+    // Guardar en Firestore
+await setDoc(doc(db, "users", user.uid), {
+  uid: user.uid,
+  email: user.email,
+  displayName: user.displayName || user.email?.split("@")[0],
+  photoURL: user.photoURL || "",
+  createdAt: new Date()
+}, { merge: true });
+
+
+  } catch (err) {
+    console.error("Error signing in with Google", err);
+  }
 };
 
-//funcion de registro en firebase
-export const signUpWithEmail= async (email: string, pass: string) => {
-    try {
-        const resCredential = await createUserWithEmailAndPassword(auth, email, pass);
-        const user = resCredential.user;
-        await setDoc(doc(db, "users", user.uid), {
-            email: user.email,
-            displayName: user.email?.split("@")[0],
-            createdAt: new Date(),
-        });
-    } catch (err) {
-        console.error("Error registering user", err);
-        alert("Error registering user");
-    }
+
+export const signUpWithEmail = async (email: string, password: string, displayName:string) => {
+  const res = await fetch('http://localhost:3000/api/users/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password , displayName })
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message || 'Error al registrar usuario');
+  }
+
+  return await res.json(); // { uid, ... }
 };
+
 
 //funcion de login con Email
 export const signInWithEmail= async (email: string, pass: string) => {
     try {
         await signInWithEmailAndPassword(auth, email, pass);
-    } catch (err) {
-        console.error("Error logging in user", err);
-        alert("Error logging in user");
-    }
+     } catch (err: any) {
+  console.error("Error logging in user", err);
+  alert(err.message); // muestra el mensaje real del error
+}
 };
 
 //funcion de logout
